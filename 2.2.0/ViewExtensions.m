dcrosta/vienna-rss

commit 6726ba2e6574d1d1c1ad0299f16e5fae0df293c1

Restore Tiger functionality for PPC users. This is also Panther-compatible, in my testing.
git-svn-id: https://vienna-rss.svn.sourceforge.net/svnroot/vienna-rss/trunk@1064 09869ea3-740d-0410-b225-97ef48a472b4

2.2.0/ViewExtensions.m
@@ -20,72 +20,49 @@
 
 #import "ViewExtensions.h"
 
-#if MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_4
-typedef enum {
-    NSAnimationEaseInOut,       // default
-    NSAnimationEaseIn,
-    NSAnimationEaseOut,
-    NSAnimationLinear
-} NSAnimationCurve;
-
-typedef enum {
-    NSAnimationBlocking,
-    NSAnimationNonblocking,
-    NSAnimationNonblockingThreaded
-} NSAnimationBlockingMode;
-
-#ifndef NSViewAnimationEndFrameKey
-	//#define NSViewAnimationEndFrameKey @"NSViewAnimationEndFrameKey"
-#endif
-#ifndef NSViewAnimationTargetKey
-	//#define NSViewAnimationTargetKey @"NSViewAnimationTargetKey"
-#endif
-
-@interface NSObject (AnimationInterfaceForCompiler)
-- (void)setAnimationBlockingMode:(NSAnimationBlockingMode)animationBlockingMode;
-- (void)setAnimationCurve:(NSAnimationCurve)curve;
-- (void)setDelegate:(id)delegate;
-- (void)setTag:(int)tag;
-- (void)setDuration:(float)duration;
-- (void)startAnimation;
-
-- (id)initWithViewAnimations:(NSArray *)animations;
-@end
-#endif
-
 @interface NSObject (ObjectWithTags)
--(void)setTag:(int)newTag;
--(int)tag;
+-(void)MA_setTag:(int)newTag;
+-(int)MA_tag;
 @end
 
 @implementation NSObject (ObjectWithTags)
 
-/* tagDict
+/* MA_tagDict
  * A dictionary used to simulate instance variables for our category
  */
-- (NSMutableDictionary *)tagDict
+- (NSMutableDictionary *)MA_tagDict
 {
 	static NSMutableDictionary *tagDict = nil;
-	if (!tagDict) tagDict = [[NSMutableDictionary alloc] init];
-
-	return tagDict;
+	NSMutableDictionary *returnDict = nil;
+	// The tagDict will only be used once.
+	if (tagDict)
+	{
+		returnDict = [tagDict autorelease];
+		tagDict = nil;
+	}
+	else
+	{
+		tagDict = [[NSMutableDictionary alloc] init];
+		returnDict = tagDict;
+	}
+	return returnDict;
 }
 
-/* setTag
+/* MA_setTag
  * Assigns the specified tag value to the animation object.
  */
--(void)setTag:(int)newTag
+-(void)MA_setTag:(int)newTag
 {
-	[[self tagDict] setObject:[NSNumber numberWithInt:newTag]
+	[[self MA_tagDict] setObject:[NSNumber numberWithInt:newTag]
 					   forKey:[NSValue valueWithPointer:self]];
 }
 
-/* tag
+/* MA_tag
  * Returns the associated tag.
  */
--(int)tag
+-(int)MA_tag
 {
-	return [[[self tagDict] objectForKey:[NSValue valueWithPointer:self]] intValue];
+	return [[[self MA_tagDict] objectForKey:[NSValue valueWithPointer:self]] intValue];
 }
 @end
 
@@ -101,14 +78,14 @@ typedef enum {
 		NSDictionary * dict = [NSDictionary dictionaryWithObjectsAndKeys:
 			[NSValue valueWithRect:newFrame], NSViewAnimationEndFrameKey,
 			self, NSViewAnimationTargetKey,
-			nil, nil];
+			nil];
 
 		id animation = [[viewAnimationClass alloc] initWithViewAnimations:[NSArray arrayWithObject:dict]];
 		[animation setAnimationBlockingMode:NSAnimationNonblocking];
 		[animation setDuration:0.1];
 		[animation setAnimationCurve:NSAnimationEaseInOut];
 		[animation setDelegate:self];
-		[animation setTag:viewTag];
+		[animation MA_setTag:viewTag];
 		[animation startAnimation];
 
 	} else {
@@ -126,7 +103,7 @@ typedef enum {
 -(void)animationDidEnd:(id)animation
 {
 	NSWindow * viewWindow = [self window];
-	int viewTag = [animation tag];
+	int viewTag = [animation MA_tag];
 
 	[animation release];
 	if ([[viewWindow delegate] respondsToSelector:@selector(viewAnimationCompleted:withTag:)])

2.2.0/AppController.m
@@ -196,9 +196,9 @@ static void MySleepCallBack(void * x, io_service_t y, natural_t messageType, voi
 	[toolbar setAllowsUserCustomization:YES];
 	[toolbar setAutosavesConfiguration:YES]; 
 	[toolbar setDisplayMode:NSToolbarDisplayModeIconOnly];
-#if (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_4)
-	[toolbar setShowsBaselineSeparator:NO];
-#endif
+	// The method setShowsBaselineSeparator does not exist in Mac OS X 10.3.9 Panther
+	if ([toolbar respondsToSelector:@selector(setShowsBaselineSeparator:)])
+		[toolbar setShowsBaselineSeparator:NO];
 	[mainWindow setToolbar:toolbar];
 
 	// Run the auto-expire now

2.2.0/ArticleListView.m
@@ -293,11 +293,10 @@ static const int MA_Minimum_Article_Pane_Width = 80;
 	
 	// In condensed mode, the summary field takes up the whole space.
 	// The method setColumnAutoresizingStyle does not exist in Mac OS X 10.3.9.
-#if (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_4)
-	[articleList setColumnAutoresizingStyle:NSTableViewUniformColumnAutoresizingStyle];
-#else
-	[articleList setAutoresizesAllColumnsToFit:YES];
-#endif
+	if ([articleList respondsToSelector:@selector(setColumnAutoresizingStyle:)])
+		[articleList setColumnAutoresizingStyle:NSTableViewUniformColumnAutoresizingStyle];
+	else
+		[articleList setAutoresizesAllColumnsToFit:YES];
 	
 	// Set up the corner button for chosing the visible columns.
 	NSMenu * columnsSubMenu = [[[NSMenu alloc] initWithTitle:@"Columns"] autorelease];
@@ -534,11 +533,10 @@ static const int MA_Minimum_Article_Pane_Width = 80;
 			[headerCell setTitle:[field displayName]];
 			[column setEditable:NO];
 			// The method setResizingMask does not exist in 10.3.9.
-#if (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_4)
-			[column setResizingMask:(isResizable ? (NSTableColumnAutoresizingMask | NSTableColumnUserResizingMask) : NSTableColumnNoResizing)];
-#else
-			[column setResizable:isResizable];
-#endif
+			if ([column respondsToSelector:@selector(setResizingMask:)])
+				[column setResizingMask:(isResizable ? (NSTableColumnAutoresizingMask | NSTableColumnUserResizingMask) : NSTableColumnNoResizing)];
+			else
+				[column setResizable:isResizable];
 			[column setMinWidth:10];
 			[column setMaxWidth:1000];
 			[column setWidth:[field width]];

2.2.0/ToolbarButton.m
@@ -42,11 +42,12 @@
 		[self setButtonType:NSMomentaryChangeButton];
 		[self setBordered:NO];
 		
-#if (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_4)
-		[self setBezelStyle:NSSmallSquareBezelStyle];
-#else
-		[self setBezelStyle:NSShadowlessSquareBezelStyle];
-#endif
+		// NSSmallSquareBezelStyle does not exist in Mac OS X 10.3.9 Panther
+		SInt32 MacVersion;
+		if (Gestalt(gestaltSystemVersion, &MacVersion) == noErr && MacVersion >= 0x1040)
+			[self setBezelStyle:NSSmallSquareBezelStyle];
+		else
+			[self setBezelStyle:NSShadowlessSquareBezelStyle];
 		
 		[self setImagePosition:NSImageOnly];
 	}

2.2.0/Vienna.xcodeproj/project.pbxproj
@@ -2525,8 +2525,8 @@
 				MACOSX_DEPLOYMENT_TARGET_i386 = 10.4;
 				MACOSX_DEPLOYMENT_TARGET_ppc = 10.3;
 				PREBINDING = NO;
+				SDKROOT = /Developer/SDKs/MacOSX10.4u.sdk;
 				SDKROOT_i386 = /Developer/SDKs/MacOSX10.4u.sdk;
-				SDKROOT_ppc = /Developer/SDKs/MacOSX10.3.9.sdk;
 				STRIP_INSTALLED_PRODUCT = NO;
 				VIENNA_VERSION_NUMBER = 2.2.0;
 			};
@@ -2743,8 +2743,8 @@
 				MACOSX_DEPLOYMENT_TARGET_i386 = 10.4;
 				MACOSX_DEPLOYMENT_TARGET_ppc = 10.3;
 				PREBINDING = NO;
+				SDKROOT = /Developer/SDKs/MacOSX10.4u.sdk;
 				SDKROOT_i386 = /Developer/SDKs/MacOSX10.4u.sdk;
-				SDKROOT_ppc = /Developer/SDKs/MacOSX10.3.9.sdk;
 				VIENNA_VERSION_NUMBER = 2.2.0;
 			};
 			name = Deployment;

2.2.0/sqlite/sqlite3.c
@@ -15488,12 +15488,6 @@ static int allocateUnixFile(
 ** Interfaces for opening a shared library, finding entry points
 ** within the shared library, and closing the shared library.
 */
-
-// This is for Mac OS X 10.3 Panther compatibility
-#ifndef DLOPEN_NO_WARN
-#define DLOPEN_NO_WARN
-#endif
-
 #include <dlfcn.h>
 static void *sqlite3UnixDlopen(const char *zFilename){
   return dlopen(zFilename, RTLD_NOW | RTLD_GLOBAL);

commit 73bb195057eaf586cee2529a68287608574a1794

Fix bug with handling animation tags introduced in r1064.
git-svn-id: https://vienna-rss.svn.sourceforge.net/svnroot/vienna-rss/trunk@1066 09869ea3-740d-0410-b225-97ef48a472b4

2.2.0/ViewExtensions.m
@@ -32,20 +32,12 @@
  */
 - (NSMutableDictionary *)MA_tagDict
 {
-	static NSMutableDictionary *tagDict = nil;
-	NSMutableDictionary *returnDict = nil;
-	// The tagDict will only be used once.
-	if (tagDict)
+	static NSMutableDictionary *MATagDict = nil;
+	if (MATagDict == nil)
 	{
-		returnDict = [tagDict autorelease];
-		tagDict = nil;
+		MATagDict = [[NSMutableDictionary alloc] init];
 	}
-	else
-	{
-		tagDict = [[NSMutableDictionary alloc] init];
-		returnDict = tagDict;
-	}
-	return returnDict;
+	return MATagDict;
 }
 
 /* MA_setTag
@@ -62,7 +54,11 @@
  */
 -(int)MA_tag
 {
-	return [[[self MA_tagDict] objectForKey:[NSValue valueWithPointer:self]] intValue];
+	NSMutableDictionary *tagDict = [self MA_tagDict];
+	NSValue *key = [NSValue valueWithPointer:self];
+	int tag = [[tagDict objectForKey:key] intValue];
+	[tagDict removeObjectForKey:key];
+	return tag;
 }
 @end
 